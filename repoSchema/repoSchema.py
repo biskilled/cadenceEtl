cfe411fec3f031acc1e9f9eef6feb5a8494a3c3f
biskilled <tal@biskilled.com>
2019-01-23 23:02:14 +0200

    go go popeye

---
+++ repoSchema/repoSchema.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append("../")
+from lib.popeye.config import config
+config.CONNECTIONS_ACTIVE = {"sql": "cnDb"}
+import lib.popeye.mapp.mapper as mapper
+
+config.CONN_URL   = {'sql' : "DRIVER={SQL Server};SERVER=XXXXX;DATABASE=YOYOYO;UID=YOYOYO;PWD=YOYOYO;",
+                     'sqlRepo': "DRIVER={SQL Server};SERVER=XXXXX,1433;DATABASE=YOYOYO;UID=YOYOYO;PWD=YOYOYO;"}
+
+config.DIR_DATA     =  "./"
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Loading data from json files, cant get: source list files or destination list files or append mode () ')
+    mapper.loadJson()
